5a9af765483980345a812743a9dbdaa2be1905ad
melkor <hohav@fastmail.com>
2021-10-03 14:57:33 -0700

    update for peppi 1.0.0-alpha.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{fs, io};
 use std::collections::HashMap;
-use arrow::array::ArrayRef;
+use arrow::array::{Array, StructArray};
 use pyo3::{
 	exceptions::PyOSError,
 	ffi::Py_uintptr_t,
@@ -17,7 +17,7 @@ fn to_py_via_json<T: serde::Serialize>(py: Python, json: &PyModule, x: &T) -> Re
 	)?.to_object(py))
 }
 
-fn to_py_via_arrow(py: Python, pyarrow: &PyModule, arr: ArrayRef) -> Result<PyObject, PyO3ArrowError> {
+fn to_py_via_arrow(py: Python, pyarrow: &PyModule, arr: StructArray) -> Result<PyObject, PyO3ArrowError> {
 	let (array_pointer, schema_pointer) = arr.to_raw()?;
 	Ok(pyarrow.getattr("Array")?.call_method1("_import_from_c",
 		(array_pointer as Py_uintptr_t, schema_pointer as Py_uintptr_t),
@@ -27,14 +27,14 @@ fn to_py_via_arrow(py: Python, pyarrow: &PyModule, arr: ArrayRef) -> Result<PyOb
 fn _game(py: Python, path: String) -> Result<PyObject, PyO3ArrowError> {
 	let pyarrow = py.import("pyarrow")?;
 	let json = py.import("json")?;
-	let game = peppi::game(&mut io::BufReader::new(fs::File::open(path)?), None)?;
+	let game = peppi::game(&mut io::BufReader::new(fs::File::open(path)?), None, None)?;
 
 	let mut m: HashMap<&str, PyObject> = HashMap::new();
 
 	m.insert("start", to_py_via_json(py, json, &game.start)?);
 	m.insert("end", to_py_via_json(py, json, &game.end)?);
 	m.insert("metadata", to_py_via_json(py, json, &game.metadata)?);
-	m.insert("frames", to_py_via_arrow(py, &pyarrow, peppi::arrow::frames(&game, None))?);
+	m.insert("frames", to_py_via_arrow(py, &pyarrow, peppi::serde::arrow::frames_to_arrow(&game, None))?);
 
 	Ok(m.to_object(py))
 }
