71f5af88c2b7cb75f56598d383e4ce2292e34b8f
melkor <hohav@fastmail.com>
2024-03-18 14:52:27 -0700

    Tell Readthedocs where to find the source

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os, sys
+
 project = 'peppi-py'
 copyright = '2024, melkor'
 author = 'melkor'
@@ -22,6 +24,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'en'
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
