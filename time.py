955dd99f44fb802d6e1b1334199d48f5ad722a18
melkor <hohav@fastmail.com>
2025-08-05 08:51:25 -0400

    Use new API

--- time.py
+++ time.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import sys, peppi_py, timeit
 
-peppi_py.game(sys.argv[1]) # warm up
-print(timeit.timeit(lambda: peppi_py.game(sys.argv[1]), number=10))
+peppi_py.read_slippi(sys.argv[1]) # warm up
+print(timeit.timeit(lambda: peppi_py.read_slippi(sys.argv[1]), number=10))
